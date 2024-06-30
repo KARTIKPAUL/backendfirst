15a308767b1d1c5676480613f5f97c20ecbd209e
KARTIKPAUL <arpilar401@gmail.com>
2024-07-01 01:10:10 +0530

    This is my first backend project

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/linkedin', (req, res) => {
+    res.send('This is My linkedin id!')
+})
+
+app.get('/facebook', (req, res) => {
+    res.send('This is My facebook id!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
